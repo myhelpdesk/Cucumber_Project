061807d2df52d482f1bf8f662d9ad4df732c8be9
ADMIN <ADMIN@DESKTOP-151NQ9B>
2024-06-19 18:29:07 +0530

    first commit

---
+++ src/test/java/testRunner/Runner.java
@@ -0,0 +1,12 @@
+package testRunner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = { "FeatureFiles/supplier.feature" }, monochrome = true, glue = {
+		"stepDefinations" }, plugin = { "pretty", "html:target/cucumber-reports.html" })
+public class Runner {
+
+}
